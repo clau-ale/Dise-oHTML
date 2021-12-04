1780c957bde6a4cfb0a017d703917b5cccbbf770
clau-ale <claudiom.ale22@gmail.com>
2021-12-04 15:17:19 -0300

    js2

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,20 @@
-class Producto{
+class Producto {
     constructor(id, nombre, precio){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
+        globalThis.id= parseInt(id);
+        globalThis.nombre = nombre;
+        globalThis.precio = precio;
     }
+}
+const productos =[];
+productos.push(new Producto(1,"Razer Kraken X USB",7000));
+productos.push(new Producto(2,"Razer Kraken WidowMaker",8000));
+productos.push(new Producto(3,"Razer Kraken X USB",7000));
+productos.push(new Producto(4,"Razer Kraken X USB",7000));
+productos.push(new Producto(5,"Razer Kraken X USB",7000));
+productos.push(new Producto(6,"Razer Kraken X USB",7000));
+console.log(productos)
+
+for (const producto of productos) {
+    alert("Prod " +productos.nombre);
+    
 }
